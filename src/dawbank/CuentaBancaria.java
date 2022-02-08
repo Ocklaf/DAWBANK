8f8866c8d9b888d57d1a387e5d9669f0b1f35673
nostromo <nostromo@nostromo-desktop>
2022-02-08 18:06:28 +0100

    DAWBANK MENÚ OPERATIVO, FALTA REVISAR 6. Retirada

--- src/dawbank/CuentaBancaria.java
+++ src/dawbank/CuentaBancaria.java
@@ -1,5 +1,7 @@
 package dawbank;
 
+import java.util.Arrays;
+
 /**
  *
  * @author nostromo
@@ -7,12 +9,69 @@ package dawbank;
 public class CuentaBancaria {
     
     private final String IBAN, NOMBRE;
+    private final double SALDOMINIMO = -50;
     private double saldo = 0;
     private double movimientos [] = new double[100];
+    private int posicionMovimientos = 0;
     
     public CuentaBancaria(String iban, String nombre){
-        this.IBAN = iban;
-        this.NOMBRE = nombre;
+        IBAN = iban;
+        NOMBRE = nombre;
     }
     
+    public String getIBAN(){
+        return IBAN;
+    }
+    
+    public String getDatosCuenta(){
+        return "IBAN: " + IBAN + "\nTitular de la cuenta: " + NOMBRE + "\nSaldo actual: " + saldo;
+    }
+    
+    public String getNOMBRE(){
+        return NOMBRE;
+    }
+    
+    public double getSaldo(){
+        return saldo;
+    }
+    
+    public void setIngreso(double ingreso){
+        if(ingreso>3000)
+            System.out.println("AVISO: Notificación a Hacienda");
+        saldo+=ingreso;
+        movimientos[posicionMovimientos] = ingreso;
+        posicionMovimientos++;
+    }
+    
+    public void setRetirada(double retirada){
+        if(saldo <= SALDOMINIMO){
+            System.out.println("AVISO: Saldo en cuenta -50€, no puede realizar más retiradas de efectivo");
+        }
+        else if(retirada - saldo < 0 && retirada - saldo > SALDOMINIMO){
+            System.out.println("AVISO: Saldo negativo");
+            saldo -= retirada;
+            movimientos[posicionMovimientos] = -retirada;
+            posicionMovimientos++;
+        }
+        else if(retirada>3000){
+            System.out.println("AVISO: Notificación a Hacienda");
+            saldo -= retirada;
+            movimientos[posicionMovimientos] = -retirada;
+            posicionMovimientos++;
+        }
+        else{
+            saldo -= retirada;
+            movimientos[posicionMovimientos] = -retirada;
+            posicionMovimientos++;
+        }
+    }
+    
+    public double[] getMovimientos(){
+        //Hago un array con los movimientos existentes y no 100 directamente por si está vacío
+        double listado[];
+        listado = Arrays.copyOf(movimientos, posicionMovimientos+1);
+        return listado;
+    }
+    
+    //public void setMovimientos()
 }
--- src/dawbank/DawBank.java
+++ src/dawbank/DawBank.java
@@ -8,6 +8,7 @@ package dawbank;
  *
  * @author nostromo
  */
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DawBank {
@@ -17,17 +18,158 @@ public class DawBank {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String iban;
-               
-        do{
-            System.out.println("Dame el IBAN de tu cuenta bancaria (2 letras más 22 números)");
+        String iban, nombreCompleto;
+        int opcion;
+
+        do {
+            System.out.println("Dame el IBAN de tu cuenta bancaria (2 letras más 22 números):");
             iban = input.nextLine();
-        }while(!verificarIBAN(iban));//While True... hemos de repetir hasta que la función nos devuelva True y hacer un NOT para salir del bucle
+        } while (!verificarIBAN(iban));//While True... hemos de repetir hasta que la función nos devuelva True y hacer un NOT para salir del bucle
         iban = iban.toUpperCase();
         //System.out.println(iban);
+
+        nombreCompleto = solicitarVerificarNombre();
+        
+        CuentaBancaria OpenBank = new CuentaBancaria(iban, nombreCompleto);
+
+        do {
+            opcion = menu();
+            menuSwitch(opcion, OpenBank);
+        } while (opcion != 8);
+    }
+
+    public static boolean verificarIBAN(String iban) {
+        return iban.matches("^[A-Za-z]{2}[0-9]{22}$+");//Que empieza y acaba por lo indicado... A-Za-z incluidos 2 carácteres al comienzo seguido de 22 dígitos y sólo una vez +
+    }
+
+    public static String solicitarVerificarNombre() {
+        Scanner input = new Scanner(System.in);
+        String nombre, apellido1, apellido2, segundo;
+
+        do {
+            System.out.println("Dígame su nombre:");
+            nombre = input.nextLine();
+        } while (nombre.length() <= 2);
+
+//        nombre = nombre.substring(0, 1).toUpperCase() + nombre.substring(1).toLowerCase();
+//        System.out.println(nombre);
+        do {
+            System.out.println("Dígame su primer apellido:");
+            apellido1 = input.nextLine();
+        } while (apellido1.length() <= 2);
+
+        do{
+            System.out.println("¿Tiene segundo apellido? S/N");
+            segundo = input.nextLine().toUpperCase();
+        }while(segundo.charAt(0) != 'S' && segundo.charAt(0) != 'N');
+        
+
+        if (segundo.charAt(0) == 'S') {
+            do {
+                System.out.println("Dígame su segundo apellido:");
+                apellido2 = input.nextLine();
+            } while (apellido2.length() <= 2);
+            return nombre + " " + apellido1 + " " + apellido2;
+        } else {
+            return nombre + " " + apellido1;
+        }
     }
     
-    public static boolean verificarIBAN(String iban){
-        return iban.matches("^[A-Za-z]{2}[0-9]{22}$+");//Que empieza y acaba por lo indicado... A-Za-z incluidos 2 carácteres al comienzo seguido de 22 dígitos y sólo una vez +
+    public static int menu(){
+        Scanner input = new Scanner(System.in);        
+        int opcion = 0;
+                do{
+            System.out.println("Elija una de las opciones disponibles:\n"
+                    + "1. Datos de la cuenta\n"
+                    + "2. IBAN\n"
+                    + "3. Titular\n"
+                    + "4. Saldo\n"
+                    + "5. Ingreso\n"
+                    + "6. Retirada\n"
+                    + "7. Movimientos\n"
+                    + "8. Salir");
+            opcion = input.nextInt();
+        }while(opcion<=1 && opcion>=8);
+        
+        return opcion;
+    }
+    
+    public static void ingresoSaldo(CuentaBancaria OpenBank){
+        Scanner input = new Scanner(System.in);
+        double ingreso;
+        int contador = 0;
+        
+        do{
+            System.out.println("Dígame el importe a ingresar mayor a 0€ (3 intentos o menú principal)");
+            ingreso = input.nextDouble();
+            contador++;
+            if(ingreso<=0)
+                System.out.println("ERROR: no puede hacer ingresos de 0€ o negativos");
+        }while(ingreso<=0 && contador<=3);
+        
+        if(ingreso>0)
+            OpenBank.setIngreso(ingreso);
+    }
+    
+        public static void retiradaSaldo(CuentaBancaria OpenBank){
+        Scanner input = new Scanner(System.in);
+        double retirada;
+        int contador = 0;
+        
+        do{
+            System.out.println("Dígame el importe a retirar debe ser mayor a 0€ (3 intentos o menú principal)");
+            retirada = input.nextDouble();
+            contador++;
+            if(retirada<=0)
+                System.out.println("ERROR: no puede hacer ingresos de 0€ o negativos");
+        }while(retirada<=0 && contador<=3);
+        
+        if(retirada>0)
+            OpenBank.setRetirada(retirada);
+    }
+        
+        public static void mostrarMovimientos(CuentaBancaria OpenBank){
+            double movimientos[];
+            int longitud;
+            
+            longitud = OpenBank.getMovimientos().length;
+            if(longitud==0){
+                System.out.println("No existen movimientos actualmente");
+            }
+            else{
+                movimientos = Arrays.copyOf(OpenBank.getMovimientos(),OpenBank.getMovimientos().length-1);
+                for(int i=0; i<movimientos.length; i++){
+                    System.out.println(movimientos[i]);
+                }
+                
+            }
+        }
+    
+    public static void menuSwitch(int opcion, CuentaBancaria OpenBank){
+        
+        
+        switch(opcion){
+            case 1:
+                System.out.println(OpenBank.getDatosCuenta());
+                break;
+            case 2:
+                System.out.println(OpenBank.getIBAN());
+                break;
+            case 3:
+                System.out.println(OpenBank.getNOMBRE());
+                break;
+            case 4: 
+                System.out.println(OpenBank.getSaldo());
+                break;
+            case 5:
+                ingresoSaldo(OpenBank);
+                break;
+            case 6:
+                retiradaSaldo(OpenBank);
+                break;
+            case 7:
+                mostrarMovimientos(OpenBank);
+                break;
+        }
     }
 }
